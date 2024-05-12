486b4e4853e25f269079fc45cd500bc72c768d8a
Dáire McDermott <dairemcdermott@outlook.com>
2024-05-12 20:29:04 +0100

    adds java version to pom.xml and fixes code smells

--- src/main/java/work/mcdermott/helloworld/commands/HelloCommand.java
+++ src/main/java/work/mcdermott/helloworld/commands/HelloCommand.java
@@ -3,37 +3,46 @@ package work.mcdermott.helloworld.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import work.mcdermott.helloworld.Main;
+
+import java.util.Objects;
 
 public class HelloCommand implements CommandExecutor {
 
-    private Main plugin;
+	public HelloCommand(Main plugin) {
+        PluginCommand helloCommand = plugin.getCommand("hello");
 
-    public HelloCommand(Main plugin) {
-        this.plugin = plugin;
-        plugin.getCommand("hello").setExecutor(this);
+        if (Objects.isNull(helloCommand)) {
+            plugin.getLogger().warning("Could not register command.");
+            return;
+        }
+
+        helloCommand.setExecutor(this);
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label,
+                             @NotNull String[] args) {
         if (!senderIsPermitted(sender)) {
             return false;
         }
 
         Player player = (Player) sender;
-        player.sendMessage("hi!");
+        player.sendMessage("Hello World!");
 
         return true;
     }
 
     private boolean senderIsPermitted(CommandSender sender) {
         if (!(sender instanceof Player)) {
-            sender.sendMessage("Only players may execute this command!");
+            sender.sendMessage("Only players may execute this command.");
             return false;
         }
         if (!sender.hasPermission("hello.use")) {
-            sender.sendMessage("You do not have permission to execute this command!");
+            sender.sendMessage("You do not have permission to execute this command.");
             return false;
         }
 
